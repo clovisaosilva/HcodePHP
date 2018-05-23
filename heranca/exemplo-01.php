928768e90c5c91d301ad2f2994d866f39c0e9bf3
Clovis Augusto <clovis.augusto@outlook.com>
2018-05-23 13:07:09 -0300

    Atualização de repositório para a aula de herança

---
+++ heranca/exemplo-01.php
@@ -0,0 +1,31 @@
+﻿<?php
+	class Documento{
+		private $numero;
+
+		public function getNumero(){
+			return $this->numero;
+		}
+
+		public function setNumero($numero){
+			$this->numero = $numero;
+		}
+	}
+
+	class CPF extends Documento{
+		public function validar():bool{
+			$numeroCPF = $this->getNumero();
+			//Validação de CPF
+			return true;
+		}
+	}
+
+	$doc = new CPF();
+
+	$doc->setNumero("028756691-38");
+
+	var_dump($doc->validar());
+
+	echo "<br/>";
+
+	echo $doc->getNumero();
+?>
