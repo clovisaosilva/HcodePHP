f4396d902096ca1d2f9a6d7ace89053aafef6bc6
Clovis Augusto <clovis.augusto@outlook.com>
2018-05-24 23:53:18 -0300

    Adicionados exemplos dos exercídios de PDO - UPDATE

---
+++ pdo/exemplo-04.php
@@ -0,0 +1,22 @@
+<?php
+	
+	/*PDO e conexão de banco de dados INSERT*/
+
+	$conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
+
+	$stmt = $conn->prepare("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID");
+
+	$login = "Jao";
+	$password = "qwerty";
+	$id = 2;
+
+	$stmt->bindParam(":LOGIN",$login);
+	$stmt->bindParam(":PASSWORD",$password);
+	$stmt->bindParam(":ID",$id);
+
+
+	$stmt->execute();
+
+	echo "Dados alterados!!!"
+
+?> 
